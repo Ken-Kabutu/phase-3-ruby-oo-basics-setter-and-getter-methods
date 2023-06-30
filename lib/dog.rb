f3b7cede18265b492f6bb94296d12275cfa24701
Ken <kabutukenneth3@gmail.com>
2023-07-01 00:12:29 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    #setter method
+    def name=(name)
+        @name = name
+    end
+
+    #getter method
+    def name
+        @name
+    end
+
+    #setter for breed
+    def breed=(name)
+        @breed = name
+    end
+
+    #getter for breed
+    def breed
+        @breed
+    end
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,29 @@
 class Person
 
+      #setter method
+      def name=(name)
+        @name = name
+    end
+
+    #getter method
+    def name
+        @name
+    end
+
+    #setter for job
+    def job=(job)
+        @job = job
+    end
+
+    #getter for job
+    def job
+        @job
+    end
+
+
 end
+
+Beyonce = Person.new
+
+Beyonce.name = "Beyonce"
+Beyonce.job = "Singer"
